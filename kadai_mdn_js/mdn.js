368d934b5cc87877db7cb08db53482db8ff46cc8
R-mariss <dd108y@yahoo.co.jp>
2024-10-25 00:59:08 +0900

    「kadai_mdn_js」アップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const moonLanding = new Date( 'september 22,23 00:00:00');
+
+console.log(moonLanding.getFullYear() +'年');
+// Expected output: 2023
+
+console.log(moonLanding.getMonth() +'月');
+// Expected output: 09(-1)
+
+console.log(moonLanding.getDate() +'日');
+// Expected output: 22
